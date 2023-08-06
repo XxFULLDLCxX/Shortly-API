82eab615e961e239f18dde8ac24cf50c6115e8c0
José Matheus <XxFULLDLCxX@outlook.com>
2023-08-06 15:03:57 -0300

    fix: correcting the url_schema

--- src/schemas/links.schemas.js
+++ src/schemas/links.schemas.js
@@ -1,5 +1,5 @@
 import Joi from "joi";
 
 export const url_schema = Joi.object({
-  url: Joi.string().pattern(/^https:\/\//).required()
+  url: Joi.string().uri({ scheme: ['http', 'https'] }).required()
 });
